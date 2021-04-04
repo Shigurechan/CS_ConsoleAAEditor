3e032de2b49e25f4249bb9591afaae48a8a64f6a
松澤賢 <yw3250118.2g2g@hotmail.com>
2021-04-04 17:22:56 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleAAEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAAEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
